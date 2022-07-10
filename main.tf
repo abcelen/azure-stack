89309bda3dccd1d9fe6348014d5686e7458d3795
Abdullah Celen <59764535+abcelen@users.noreply.github.com>
2022-07-10 16:08:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,12 +41,12 @@ module "compute" {
   app_os_password = var.app_os_password
 }
 
-module "database" {
-  source = "./modules/database"
-  location = module.resourcegroup.location_id
-  resource_group = module.resourcegroup.resource_group_name
-  primary_database = var.primary_database
-  primary_database_version = var.primary_database_version
-  primary_database_admin = var.primary_database_admin
-  primary_database_password = var.primary_database_password
-}
+# module "database" {
+#   source = "./modules/database"
+#   location = module.resourcegroup.location_id
+#   resource_group = module.resourcegroup.resource_group_name
+#   primary_database = var.primary_database
+#   primary_database_version = var.primary_database_version
+#   primary_database_admin = var.primary_database_admin
+#   primary_database_password = var.primary_database_password
+# }
